d473f204e1bcfb7f652f142eabb93a7651f5dc58
hejiavenues <yanliping12308@163.com>
2021-02-22 10:37:44 +0800

    图片压缩工具类

--- src/main/java/cn/cashbang/core/common/utils/CommonUtils.java
+++ src/main/java/cn/cashbang/core/common/utils/CommonUtils.java
@@ -1,19 +1,29 @@
 package cn.cashbang.core.common.utils;
 
-import cn.cashbang.core.common.constant.MsgConstant;
-import cn.cashbang.core.common.constant.SystemConstant;
-import cn.cashbang.core.common.entity.Result;
-
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
+import javax.imageio.ImageIO;
+
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+
+import cn.cashbang.core.common.constant.MsgConstant;
+import cn.cashbang.core.common.constant.SystemConstant;
+import cn.cashbang.core.common.entity.Result;
 
 /**
  * 通用工具类
@@ -144,4 +154,58 @@ public class CommonUtils {
 	        }
 	        return sortedJsonArray.toString();
 	    }
+	 
+	 public static Boolean zipWidthHeightImageFile(String oldPath, String copyPath, int width, int height,
+             float quality) {
+		 if(width == 0) {
+			 width = 800;
+		 }
+		 if(height == 0) {
+			 width = 256;
+		 }
+		 if(quality == 0) {
+			 quality = 6f;
+		 }
+		 Boolean sta = false;
+		 File oldFile = new File(oldPath);
+		 File newFile = new File(copyPath);
+		 if (oldFile == null) {
+			 return null;
+		 }
+		 String newImage = null;
+		 try {
+			 /** 对服务器上的临时文件进行处理 */
+			 Image srcFile = ImageIO.read(oldFile);
+			 int w = srcFile.getWidth(null);
+			 System.out.println(w);
+			 int h = srcFile.getHeight(null);
+			 System.out.println(h);
+
+			 /** 宽,高设定 */
+			 BufferedImage tag = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			 tag.getGraphics().drawImage(srcFile, 0, 0, width, height, null);
+			 //String filePrex = oldFile.substring(0, oldFile.indexOf('.'));
+			 /** 压缩后的文件名 */
+			 //newImage = filePrex + smallIcon+ oldFile.substring(filePrex.length());
+			 
+			 /** 压缩之后临时存放位置 */
+			 FileOutputStream out = new FileOutputStream(newFile);
+
+			 JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+			 JPEGEncodeParam jep = JPEGCodec.getDefaultJPEGEncodeParam(tag);
+			 /** 压缩质量 */
+			 jep.setQuality(quality, true);
+			 encoder.encode(tag, jep);
+			 out.close();
+			 sta = true;
+		 } catch (Exception e) {
+			 e.printStackTrace();
+			 sta = false;
+		 }
+		 return sta;
+	 }
+	 
+	 public static void main(String args[]) throws IOException { 
+	        System.out.println(CommonUtils.zipWidthHeightImageFile("E:/piture01/71ba7d659fed43c988bd243cdefa7185-2021_02_19_15_17_43_124.png","E:/piture01/c2.png", 800, 256, 6f)); 
+	  }
 }
